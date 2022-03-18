1b2e2dd44f0ffeb228b99efb6f40cf37e3252800
Mark Miranda <mmiranda@doximity.com>
2022-03-18 13:36:03 -0600

    Add MVP calculator

--- src/components/GuessInputField.vue
+++ src/components/GuessInputField.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="col-1">
     <label><strong>{{ label }}</strong></label>
-    <input type="text" class="form-control form-control-lg">
+    <input
+      :ref="refName"
+      type="number"
+      placeholder="0"
+      v-model="inputVal"
+      @focus="$event.target.select()"
+      class="form-control form-control-lg"
+    >
   </div>
 </template>
 
@@ -9,10 +16,46 @@
 export default {
   name: 'GuessInputField',
   props: {
+    value: {
+      type: Number,
+      default: 0
+    },
     label: {
       type: String,
       required: true,
     },
+    index: {
+      type: Number,
+      required: true,
+    }
+  },
+  computed: {
+    inputVal: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        console.log('HELP', val);
+        this.$emit('input', {val: val, index: this.index})
+      }
+    },
+    refName() {
+      if (this.index == 0) {
+        return 'firstInput'
+      } else {
+        return 'other'
+      }
+    }
+  },
+  methods: {
+    focusInput() {
+      if (this.$refs.firstInput) {
+        this.$refs.firstInput.focus()
+      }
+    }
+  },
+  mounted() {
+    this.focusInput()
   }
 }
 </script>
--- src/components/WordleAverager.vue
+++ src/components/WordleAverager.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="mt-4 text-center">
     <h1>Wordle Averager!</h1>
+    <hr />
     <h3>Guess Distribution</h3>
-    <div class="row justify-content-center">
-      <guess-input-field label="1" />
-      <guess-input-field label="2" />
-      <guess-input-field label="3" />
-      <guess-input-field label="4" />
-      <guess-input-field label="5" />
-      <guess-input-field label="6" />
+    <div class="row justify-content-center mb-4">
+      <guess-input-field
+        v-for="(guess, index) in guessDistribution"
+        :key="index"
+        :index="index"
+        :label="(index + 1).toString()"
+        :value="guessDistribution[index]"
+        @input="setGuess"
+      />
+    </div>
+    <div class="col">
+      <h3>Total Number of successful plays: {{ numberOfPlays }}</h3>
+      <h3>Total Number of guesses: {{ sumOfGuesses }}</h3>
+      <h3>Average number of guesses: {{average}}</h3>
     </div>
   </div>
 </template>
@@ -19,5 +27,33 @@ import GuessInputField from './GuessInputField.vue'
 export default {
   name: 'WordleAverager',
   components: { GuessInputField },
+  data() {
+    return {
+      guessDistribution: [2, 4, 16, 18, 8, 3]
+    }
+  },
+  computed: {
+    numberOfPlays() {
+      return this.guessDistribution.reduce((acc, val) => acc + val, 0)
+    },
+    sumOfGuesses() {
+      return this.guessDistribution.reduce((acc, val, index) => acc + ((index + 1) * val), 0)
+    },
+    average() {
+      return (this.sumOfGuesses / this.numberOfPlays).toFixed(3)
+    },
+    computeRef(index) {
+      if (index==0) {
+        return 'first'
+      } else {
+        return 'other'
+      }
+    }
+  },
+  methods: {
+    setGuess(obj) {
+      this.guessDistribution[obj.index] = obj.val
+    },
+  },
 }
 </script>
